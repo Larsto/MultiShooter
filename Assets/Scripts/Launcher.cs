4406a8f106d31178a824b5aa3fc74135af7f52c2
Jussi <37219216+Larsto@users.noreply.github.com>
2021-07-15 16:43:00 +0300

    Photon

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -28,6 +28,7 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public GameObject roomBrowserScreen;
     public RoomButton theRoomButton;
+    private List<RoomButton> allRoomButtons = new List<RoomButton>();
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +48,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         createRoomScreen.SetActive(false);
         roomScreen.SetActive(false);
         errorScreen.SetActive(false);
+        roomBrowserScreen.SetActive(false);
     }
 
     public override void OnConnectedToMaster()
@@ -117,5 +119,40 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         CloseMenus();
         menuButtons.SetActive(true);
+    }
+
+    public void OpenRoomBrowser()
+    {
+        CloseMenus();
+        roomBrowserScreen.SetActive(true);
+    }
+
+    public void CloseRoomBrowser()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(RoomButton rb in allRoomButtons)
+        {
+            Destroy(rb.gameObject);
+        }
+        allRoomButtons.Clear();
+
+        theRoomButton.gameObject.SetActive(false);
+
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            if(roomList[i].PlayerCount != roomList[i].MaxPlayers && !roomList[i].RemovedFromList)
+            {
+                RoomButton newButton = Instantiate(theRoomButton, theRoomButton.transform.parent);
+                newButton.SetButtonDetails(roomList[i]);
+                newButton.gameObject.SetActive(true);
+
+                allRoomButtons.Add(newButton);
+            }
+        }
     }
 }
